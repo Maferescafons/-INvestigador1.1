2190f068c065ef09de26d765ae8d7dca16988699
Maferescafons <escafons@hotmail.com>
2017-08-28 16:18:58 -0500

    busqueda y recuperacion de articulos

---
+++ busqueda_articulos/api/models/Articulo.js
@@ -0,0 +1,41 @@
+module.exports = {
+    attributes: {
+        title: {
+            type: "string"
+        },
+        country: {
+            type: "string"
+        },
+        number: {
+            type: "string"
+        },
+        volume: {
+            type: "string"
+        },
+        year: {
+            type: "string"
+        },
+        abstract: {
+            type: "string"
+        },
+        issns: {
+            type: "string"
+        },
+        language: {
+            type: "string"
+        },
+        keywords: {
+            type: "string"
+        },
+        link: [
+            {
+                url: "string"
+            }
+        ],
+        author: [
+            {
+                name: "string"
+            }
+        ],
+    }
+};
